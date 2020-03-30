a04ba8a7ec5616e0df6c5b3357b8fcc7dd009ce4
Jason Lane <jason.lane@thentwrk.com>
2020-03-30 10:15:51 -0700

    blah

--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module github.com/devblueray/devops-monitoring-poc
 go 1.14
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.4.1 // indirect
 	github.com/cockroachdb/cockroach-go v0.0.0-20200312223839-f565e4789405 // indirect
 	github.com/felixge/httpsnoop v1.0.1 // indirect
+	github.com/go-sql-driver/mysql v1.5.0
 	github.com/gobuffalo/envy v1.9.0 // indirect
 	github.com/gobuffalo/fizz v1.9.8 // indirect
 	github.com/gobuffalo/flect v0.2.1 // indirect
@@ -16,6 +18,7 @@ require (
 	github.com/gorilla/mux v1.7.4
 	github.com/honeycombio/beeline-go v0.4.11
 	github.com/honeycombio/libhoney-go v1.12.3 // indirect
+	github.com/jmoiron/sqlx v1.2.0
 	github.com/julienschmidt/httprouter v1.3.0 // indirect
 	github.com/labstack/echo v3.3.10+incompatible // indirect
 	github.com/labstack/gommon v0.3.0 // indirect
--- main.go
+++ main.go
@@ -2,59 +2,122 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
-	"math/rand"
 	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	beeline "github.com/honeycombio/beeline-go"
+	"github.com/honeycombio/beeline-go/wrappers/hnygorilla"
 	"github.com/honeycombio/beeline-go/wrappers/hnynethttp"
 	"github.com/honeycombio/beeline-go/wrappers/hnysqlx"
 	"github.com/jmoiron/sqlx"
 )
 
+func HelloWorldHelper(ctx context.Context) string {
+	ctx, span := beeline.StartSpan(ctx, "HelloHelper")
+	defer span.Send()
+	return "Hello World"
+}
 func HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
-	beeline.AddField(r.Context(), "hello", rand.Intn(1000))
-	fmt.Fprintln(w, "Hello World")
+	fmt.Fprintln(w, HelloWorldHelper(r.Context()))
+}
+
+func ThirdPartyAPIHandler(w http.ResponseWriter, r *http.Request) {
+	ctx, span := beeline.StartSpan(r.Context(), "staging ping")
+	defer span.Send()
+	resp, err := http.Get("http://staging.droplive.com/api/v1/pingall")
+	if err != nil {
+		beeline.AddField(ctx, "error", err)
+		fmt.Println(err)
+	}
+	apiResponse, _ := json.Marshal(resp)
+	w.Write(apiResponse)
 }
 func AddCustomerHandler(w http.ResponseWriter, r *http.Request) {
-
-	odb, err := sqlx.Connect("mysql", "root:abc123@(localhost:3306)/foo")
+	// ctx, span := beeline.StartSpan(context.Background(), "start")
+	// defer span.Send()
+	type Customer struct {
+		Name    string
+		Address string
+	}
+	var c Customer
+	err := json.NewDecoder(r.Body).Decode(&c)
 	if err != nil {
 		fmt.Println(err)
 	}
+	odb, err := sqlx.Connect("mysql", "root:abc123@(localhost:3306)/foo")
+	if err != nil {
+		beeline.AddFieldToTrace(r.Context(), "error", err)
+		fmt.Println(err)
+	}
 	db := hnysqlx.WrapDB(odb)
-	ctx, span := beeline.StartSpan(context.Background(), "start")
-	defer span.Send()
+	defer db.Close()
+
 	query := `INSERT INTO customers(name,address) VALUES (?,?)`
-	db.MustExecContext(ctx, query, "Random Name", "123 4th St Los Angeles CA 90505")
-	db.Close()
-
+	db.MustExecContext(r.Context(), query, c.Name, c.Address)
 }
-
+func Uinttest(w http.ResponseWriter, r *http.Request) {
+	ctx, span := beeline.StartSpan(context.Background(), "Uint64Test")
+	defer span.Send()
+	odb, err := sqlx.Connect("mysql", "root:abc123@(localhost:3306)/foo")
+	if err != nil {
+		beeline.AddFieldToTrace(ctx, "error", err)
+		fmt.Println(err)
+	}
+	db := hnysqlx.WrapDB(odb)
+	defer db.Close()
+	var result uint64
+	err = db.GetContext(ctx, &result, "SELECT ?", ^uint64(0))
+	if err != nil {
+		beeline.AddFieldToTrace(ctx, "error", err)
+		fmt.Println(err)
+	}
+	fmt.Println(result)
+}
 func GetCustomersHandler(w http.ResponseWriter, r *http.Request) {
 
 	type Customers struct {
-		name    string
-		address string
+		ID      string
+		Name    string
+		Address string
 	}
+	c := []Customers{}
 
-	ctx, span := beeline.StartSpan(context.Background(), "GetCustomersQuery")
+	ctxQuery, span := beeline.StartSpan(r.Context(), "GetCustomersQuery")
 	defer span.Send()
+	ctxPing, span := beeline.StartSpan(ctxQuery, "staging ping")
+	defer span.Send()
+	_, err := http.Get("http://staging.droplive.com/api/v1/pingall")
+	if err != nil {
+		beeline.AddField(ctxPing, "error", err)
+	}
 	odb, err := sqlx.Connect("mysql", "root:abc123@(localhost:3306)/foo")
 	if err != nil {
 		fmt.Println(err)
+		beeline.AddFieldToTrace(r.Context(), "error", err)
 	}
 	db := hnysqlx.WrapDB(odb)
-	rows, _ := db.QueryxContext(ctx, "SELECT * FROM customers")
-	for rows.Next() {
-		var c Customers
-		err = rows.StructScan(&c)
-		fmt.Println(c.name, c.address)
+	//defer db.Close()
+	err = db.SelectContext(ctxQuery, &c, "SELECT * FROM customers where name = 'nsuoziwfzq'")
+	if err != nil {
+		beeline.AddFieldToTrace(ctxQuery, "error", err)
+		fmt.Println(err)
 	}
+	customerJSON, err := json.Marshal(c)
+	if err != nil {
+		beeline.AddFieldToTrace(r.Context(), "error", err)
+		fmt.Println(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(customerJSON)
 
+	for _, customer := range c {
+		fmt.Println(customer.Name)
+	}
 }
 func main() {
 	beeline.Init(beeline.Config{
@@ -63,9 +126,12 @@ func main() {
 	})
 	defer beeline.Close()
 	r := mux.NewRouter()
+	r.Use(hnygorilla.Middleware)
 	r.HandleFunc("/hello", HelloWorldHandler)
 	r.HandleFunc("/create", AddCustomerHandler)
 	r.HandleFunc("/read", GetCustomersHandler)
+	r.HandleFunc("/test", Uinttest)
+	r.HandleFunc("/apitest", ThirdPartyAPIHandler)
 	fmt.Println("Starting on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", hnynethttp.WrapHandler(r)))
 }
